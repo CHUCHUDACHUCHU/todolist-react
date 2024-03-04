2b7ca1ad5c682cbab829f2b7061f232ee58f10a6
추교윤 <chu.gyoyoon@mkinternet.com>
2024-03-04 16:38:23 +0900

    todolist

---
+++ src/components/TodoList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import TodoItem from './TodoItem';
+import { useTodoState } from '../TodoContext';
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+`;
+
+function TodoList() {
+  const todos = useTodoState();
+  console.log(todos);
+  return (
+    <TodoListBlock>
+      {todos.map((todo) => {
+        console.log(todo.done);
+        return (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            text={todo.text}
+            done={todo.done.toString()}
+          />
+        );
+      })}
+    </TodoListBlock>
+  );
+}
+
+export default TodoList;
